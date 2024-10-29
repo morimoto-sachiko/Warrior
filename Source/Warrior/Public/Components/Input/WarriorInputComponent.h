bcb3eff1015fb66167f966c7975369d689b9ca99
sachiko morimoto <morimoto.sachiko@gmail.com>
2024-10-29 19:33:34 +0900

    13. Custom Enhanced Input Component

---
+++ Source/Warrior/Public/Components/Input/WarriorInputComponent.h
@@ -0,0 +1,30 @@
+// Copyright 2024 Morimoto. All Rights Reserved. This project is for personal study purposes only.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "WarriorInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+template<class UserObject, typename CallbackFunc>
+inline void UWarriorInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input config data asset is null,can not proceed with binding"));
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
